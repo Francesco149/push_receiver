41058b8857a4a7856e70b94c645970855a4a171b
Franc[e]sco <lolisamurai@tfwno.gf>
2019-11-15 21:12:01 +0100

    gitignore pyc, fix upload step

--- publish.sh
+++ publish.sh
@@ -2,7 +2,6 @@
 
 rm -rf dist/
 rm -rf build/
-python2 setup.py clean sdist bdist_wheel &&
-python3 setup.py clean sdist bdist_wheel &&
-python3 -m twine upload \
-  --repository-url https://test.pypi.org/legacy/ dist/*
+python2 setup.py clean sdist bdist_wheel --universal &&
+python3 setup.py clean sdist bdist_wheel --universal &&
+python3 -m twine upload dist/*
